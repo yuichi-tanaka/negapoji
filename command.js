c8a3da4f12cdc403d02b11569a6e5745a73ee05e
yuichi-tanaka <tanaka.y.p@gmail.com>
2016-08-25 09:51:45 +0900

    add eslint,fix eslint error

--- command.js
+++ command.js
@@ -1,41 +1,33 @@
-
-const kuromoji = require('kuromoji'),
-  path = require('path'),
-  fs = require('fs'),
-  dic_path = '/node_modules/kuromoji/dict',
-  dic_dir = path.normalize(__dirname + dic_path);
-
 const negapoji = require('./libs/negapoji');
 
 const argv = process.argv;
 
 const _utils = {
-  exit(code, msg) {
+  exit (code, msg) {
     msg = typeof msg === 'undefined' ? 'unset error message' : msg;
     console.error(msg);
     process.exit(code);
   },
-  shift(num) {
+  shift (num) {
     num = typeof num === 'undefined' ? 1 : num;
-    if(!(this instanceof Array)){
-      _utils.exit(1,'usage _utils.shift.call(<Array>,num)');
+    if (!(this instanceof Array)) {
+      _utils.exit(1, 'usage _utils.shift.call(<Array>,num)');
     }
-    for(var i = num; i--; this.shift());
-  },
+    for (var i = num; i--; this.shift());
+  }
 };
 
-  if(argv.length <= 2){
-    _utils.exit(1,'usege: node command.js すもももももももものうち');
-  }
-  _utils.shift.call(argv,2);
+if (argv.length <= 2) {
+  _utils.exit(1, 'usege: node command.js すもももももももものうち');
+}
+_utils.shift.call(argv, 2);
 
-  const txt = argv[0];
+const txt = argv[0];
 
-
-negapoji.build({},(err, build_obj) => {
-  if(err){
+negapoji.build({}, (err, build_obj) => {
+  if (err) {
     throw err;
   }
   var res = build_obj.calc(txt);
-  console.log('score : ', res , ' n/p :' , (res >= 0) ?  'positive' : 'negative');
+  console.log('score : ', res, ' n/p :', (res >= 0) ? 'positive' : 'negative');
 });
--- libs/negapoji.js
+++ libs/negapoji.js
@@ -15,19 +15,18 @@ const negapoji = () => {};
  * - opts.kuromoji_dic
  */
 const kuromoji_build = (opts) => {
-  var opts = opts || {};
+  opts = opts || {};
 
   const dic_path = '/../node_modules/kuromoji/dict',
-  dic_dir = path.normalize(__dirname + dic_path),
-  _filter = typeof opts.filter !== 'undefined' ? (val) => (val.pos === '名詞' || val.pos === '動詞' || val.pos === '形容詞' || val.pos === '副詞') : opts.filter,
-  _kuromoji_dic = typeof opts.kuromoji_dic === 'undefined' ? dic_dir : opts.kuromoji_dic;
+    dic_dir = path.normalize(path.join(__dirname, dic_path)),
+    _kuromoji_dic = typeof opts.kuromoji_dic === 'undefined' ? dic_dir : opts.kuromoji_dic;
 
-  return new Promise((res,rej) => {
+  return new Promise((resolve, reject) => {
     kuromoji.builder({dicPath: _kuromoji_dic}).build((err, tokenizer) => {
       if (err) {
-        rej(err);
+        reject(err);
       }
-      res(tokenizer);
+      resolve(tokenizer);
     });
   });
 };
@@ -37,15 +36,14 @@ const kuromoji_build = (opts) => {
  * - opts.
  */
 const sentiment_build = (opts) => {
-
   /**
    * default_parser_noun
    */
   const default_parser_noun = (rows, dict) => {
-    var dict = dict || {};
+    dict = dict || {};
     rows.map((row) => {
-      const fields = row.split("\t"),
-      v = fields[1] === 'p' ? 1 : fields[1] === 'n' ? -1 : 0;
+      const fields = row.split('\t'),
+        v = fields[1] === 'p' ? 1 : fields[1] === 'n' ? -1 : 0;
       dict[fields[0]] = [fields[0], v, fields[2]];
     });
     return dict;
@@ -55,11 +53,11 @@ const sentiment_build = (opts) => {
    * default_parser_term
    */
   const default_parser_term = (rows, dict) => {
-    var dict = dict || {};
+    dict = dict || {};
     rows.map((row) => {
-      const fields = row.split("\t"),
-      pos = (fields[0].indexOf('ポジ') >= 0) ? 1 : -1,
-      env = (fields[0].indexOf('評価') >= 0) ? '評価' : '経験';
+      const fields = row.split('\t'),
+        pos = (fields[0].indexOf('ポジ') >= 0) ? 1 : -1,
+        env = (fields[0].indexOf('評価') >= 0) ? '評価' : '経験';
 
       dict[fields[1]] = [fields[1], pos, env];
     });
@@ -79,24 +77,24 @@ const sentiment_build = (opts) => {
    * file reader
    */
   const read = (files, dict, cb) => {
-    if(files.length <= 0) {
+    if (files.length <= 0) {
       return cb(dict);
     }
-    const _obj = files.pop();
-    const file = path.normalize(__dirname + _default  + _obj.file);
+    const _obj = files.pop(),
+      file = path.normalize(path.join(__dirname, _default, _obj.file));
     dict = (dict instanceof Object) ? dict : {};
-    fs.readFile(file, (e, data) =>{
+    fs.readFile(file, (e, data) => {
       if (e) {
         throw e;
       }
-      _obj.parser(data.toString().split("\n"), dict);
+      _obj.parser(data.toString().split('\n'), dict);
       read(files, dict, cb);
     });
   };
 
-  return new Promise((res, rej) => {
-    read(dic_files,[],(dict) => {
-      res(dict);
+  return new Promise((resolve, reject) => {
+    read(dic_files, [], (dict) => {
+      resolve(dict);
     });
   });
 };
@@ -104,36 +102,37 @@ const sentiment_build = (opts) => {
 /**
  * add_sentiment_param
  */
-const add_sentiment_param = (dict,words) => {
-      words.filter((word) => dict[word.basic_form || word.surface_form]).map((word) => {
-        word.sc = dict[word.basic_form || word.surface_form];
-      });
-      return words;
+const add_sentiment_param = (dict, words) => {
+  words.filter((word) => dict[word.basic_form || word.surface_form]).map((word) => {
+    word.sc = dict[word.basic_form || word.surface_form];
+  });
+  return words;
 };
 
 /**
  * calculate
  */
 const calculate = (words) => {
-      var filter_val = words.filter((word) => word.sc);
-      var total_score = filter_val.map((word) => {
-        return word.sc[1];
-      }).reduce((x, y) => x + y);
+  var filter_val = words.filter((word) => word.sc),
+    total_score = filter_val.map((word) => {
+      return word.sc[1];
+    }).reduce((x, y) => x + y);
 
-      return total_score/filter_val.length;
+  return total_score / filter_val.length;
 };
 
 /**
  * build
  */
-negapoji.build = (opts,cb) => {
+negapoji.build = (opts, cb) => {
   Promise.all([kuromoji_build(), sentiment_build()]).then((result) => {
-    const tokenizer = result[0];
-    const sentiment_dic = result[1];
-    cb(null,{
-      calc(txt) {
-        var words = tokenizer.tokenize(txt);
-        words = add_sentiment_param(sentiment_dic,words)
+    const tokenizer = result[0],
+      sentiment_dic = result[1],
+      _filter = typeof opts.filter === 'undefined' ? (val) => (val.pos === '名詞' || val.pos === '動詞' || val.pos === '形容詞' || val.pos === '副詞') : opts.filter;
+    cb(null, {
+      calc (txt) {
+        var words = tokenizer.tokenize(txt).filter(_filter);
+        words = add_sentiment_param(sentiment_dic, words);
         return calculate(words);
       }
     });
